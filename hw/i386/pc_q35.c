6ad82d225ad1449435a3521615fe0f9638a9651a
Stefan Berger <stefanb@linux.vnet.ibm.com>
2018-01-19 16:46:04 -0500

    q35: Allow tpm-crb as a dynamic sysbus device
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -50,6 +50,7 @@
 #include "hw/usb.h"
 #include "qemu/error-report.h"
 #include "sysemu/numa.h"
+#include "sysemu/tpm.h"
 
 /* ICH9 AHCI has 6 ports */
 #define MAX_SATA_PORTS     6
@@ -303,6 +304,7 @@ static void pc_q35_machine_options(MachineClass *m)
     m->no_floppy = 1;
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_AMD_IOMMU_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_INTEL_IOMMU_DEVICE);
+    machine_class_allow_dynamic_sysbus_dev(m, TYPE_TPM_CRB);
     m->max_cpus = 288;
 }
 
